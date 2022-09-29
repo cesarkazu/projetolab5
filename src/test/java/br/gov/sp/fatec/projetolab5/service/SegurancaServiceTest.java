609fe9ffb43500a144de28956d6c258a80ddeb26
CesarKazu <andocesar@yahoo.com.br>
2022-09-29 22:38:51 +0000

    testes de integração

--- src/test/java/br/gov/sp/fatec/projetolab5/service/SegurancaServiceTest.java
+++ src/test/java/br/gov/sp/fatec/projetolab5/service/SegurancaServiceTest.java
@@ -1,6 +1,9 @@
 package br.gov.sp.fatec.projetolab5.service;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 
 import br.gov.sp.fatec.projetolab5.entity.Usuario;
 import br.gov.sp.fatec.projetolab5.repository.UsuarioRepository;
@@ -28,11 +31,29 @@ public class SegurancaServiceTest {
     usuario.setNome("Teste");
     usuario.setSenha("Senha");
     Optional<Usuario> usuarioOp = Optional.of(usuario);
-    Mockito.when(usuarioRepo.findById(usuario.getId())).thenReturn(usuarioOp);
+    Mockito.when(usuarioRepo.findById(any())).thenReturn(usuarioOp);
+    Mockito.when(usuarioRepo.save(any())).thenReturn(usuario);
   }
 
   @Test
   public void buscarUsuarioPorIdTestOk() {
     assertEquals(service.buscarUsuarioPorId(1L).getNome(), "Teste");
+  }
+
+  @Test
+  public void novoUsuarioTestNOkNomeNull() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> {
+        service.novoUsuario(null, "Senha");
+      }
+    );
+  }
+
+  @Test
+  public void novoUsuarioTestOk() {
+    assertDoesNotThrow(() -> {
+      service.novoUsuario("Teste", "Senha");
+    });
   }
 }
