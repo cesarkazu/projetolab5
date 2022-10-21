44cbf7794fe9a8bb576d7e468a7e6d43344f0096
CesarKazu <andocesar@yahoo.com.br>
2022-10-21 00:19:17 +0000

    correção controller autorização

--- src/test/java/br/gov/sp/fatec/projetolab5/controller/AutorizacaoControllerTest.java
+++ src/test/java/br/gov/sp/fatec/projetolab5/controller/AutorizacaoControllerTest.java
@@ -16,7 +16,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(UsuarioController.class)
+@WebMvcTest(AutorizacaoController.class)
 public class AutorizacaoControllerTest {
 
   @Autowired
