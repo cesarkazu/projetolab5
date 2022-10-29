9bda1b4286aa3f87830ca2b02ca5aef9360978a4
CesarKazu <andocesar@yahoo.com.br>
2022-10-29 03:12:38 +0000

    testes no service e controller de anotação

---
+++ src/test/java/br/gov/sp/fatec/projetolab5/controller/AnotacaoControllerTest.java
@@ -0,0 +1,60 @@
+package br.gov.sp.fatec.projetolab5.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import br.gov.sp.fatec.projetolab5.entity.Anotacao;
+import br.gov.sp.fatec.projetolab5.entity.Usuario;
+import br.gov.sp.fatec.projetolab5.service.AnotacaoService;
+
+@WebMvcTest(AnotacaoController.class)
+public class AnotacaoControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AnotacaoService service;
+
+    @Test
+    public void buscaAnotacoesTestOk() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNome("teste usuario");
+        usuario.setSenha("senha usuario");
+
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(1L);
+        anotacao.setTexto("teste anotacao");
+        anotacao.setDataHora(new Date());
+        anotacao.setUsuario(usuario);
+
+        List<Anotacao> anotacoes = new ArrayList<Anotacao>();
+        anotacoes.add(anotacao);
+
+        Mockito.when(service.buscaAnotacoes("teste")).thenReturn(anotacoes);
+
+        mvc.perform(get("/anotacao/teste")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].id").value(1L));
+    }
+    
+}
---
+++ src/test/java/br/gov/sp/fatec/projetolab5/service/AnotacaoServiceTest.java
@@ -0,0 +1,173 @@
+package br.gov.sp.fatec.projetolab5.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import br.gov.sp.fatec.projetolab5.entity.Usuario;
+import br.gov.sp.fatec.projetolab5.entity.Anotacao;
+import br.gov.sp.fatec.projetolab5.repository.AnotacaoRepository;
+
+@SpringBootTest
+public class AnotacaoServiceTest {
+
+    @Autowired
+    private AnotacaoService service;
+
+    @MockBean
+    private AnotacaoRepository anotacaoRepo;
+
+    @BeforeEach
+    public void setUp() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNome("teste usuario");
+        usuario.setSenha("senha usuario");
+
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(1L);
+        anotacao.setTexto("teste anotacao");
+        anotacao.setDataHora(new Date());
+        anotacao.setUsuario(usuario);
+
+        List<Anotacao> anotacoes = new ArrayList<Anotacao>();
+        anotacoes.add(anotacao);
+
+        Mockito.when(anotacaoRepo.findAll()).thenReturn(anotacoes);
+        Mockito.when(anotacaoRepo.findByTextoContains(any())).thenReturn(anotacoes);
+        Mockito.when(anotacaoRepo.save(any())).thenReturn(anotacao);
+    }
+
+    @Test
+    public void buscaAnotacoesTestOkTextoNull() {
+        assertEquals(1, service.buscaAnotacoes(null).size());
+    }
+
+    @Test
+    public void buscaAnotacoesTestOkTextoVazio() {
+        assertEquals(1, service.buscaAnotacoes("").size());
+    }
+
+    @Test
+    public void buscaAnotacoesTestOkComTexto() {
+        assertEquals(1, service.buscaAnotacoes("teste").size());
+    }
+
+    @Test
+    public void novaAnotacaoTestOkTextoNull() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(1L);
+        anotacao.setTexto(null);
+        anotacao.setDataHora(new Date());
+        anotacao.setUsuario(usuario);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.novaAnotacao(anotacao);
+        });
+    }
+
+    @Test
+    public void novaAnotacaoTestOkTextoVazio() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(1L);
+        anotacao.setTexto("");
+        anotacao.setDataHora(new Date());
+        anotacao.setUsuario(usuario);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.novaAnotacao(anotacao);
+        });
+    }
+
+    @Test
+    public void novaAnotacaoTestOkUsuarioNulo() {
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(1L);
+        anotacao.setTexto("teste anotacao");
+        anotacao.setDataHora(new Date());
+        anotacao.setUsuario(null);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.novaAnotacao(anotacao);
+        });
+    }
+
+    @Test
+    public void novaAnotacaoTestOkUsuarioIdNulo() {
+        Usuario usuario = new Usuario();
+        usuario.setId(null);
+
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(1L);
+        anotacao.setTexto("teste anotacao");
+        anotacao.setDataHora(new Date());
+        anotacao.setUsuario(usuario);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.novaAnotacao(anotacao);
+        });
+    }
+
+    @Test
+    public void novaAnotacaoTestOkUsuarioNaoEncontrado() {
+        Usuario usuario = new Usuario();
+        usuario.setId(9999L);
+
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(1L);
+        anotacao.setTexto("teste anotacao");
+        anotacao.setDataHora(new Date());
+        anotacao.setUsuario(usuario);
+
+        assertThrows(RuntimeException.class, () -> {
+            service.novaAnotacao(anotacao);
+        });
+    }
+
+    @Test
+    public void novaAnotacaoTestOkDataHoraNula() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(1L);
+        anotacao.setTexto("teste anotacao");
+        anotacao.setDataHora(null);
+        anotacao.setUsuario(usuario);
+
+        Anotacao anotacaoRetornada = service.novaAnotacao(anotacao);
+        assertNotNull(anotacaoRetornada.getDataHora());
+    }
+
+    @Test
+    public void novaAnotacaoTestOk() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+
+        Anotacao anotacao = new Anotacao();
+        anotacao.setId(1L);
+        anotacao.setTexto("teste anotacao");
+        anotacao.setDataHora(null);
+        anotacao.setUsuario(usuario);
+
+        assertEquals("teste anotacao", service.novaAnotacao(anotacao).getTexto());
+    }
+}
